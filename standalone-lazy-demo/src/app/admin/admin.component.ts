3bf5ceeabcf8560bfca915a543e33a4b8e420070
nafizur.rahman@brainstation-23.com <nafizur.rahman@brainstation-23.com>
2025-01-27 14:11:24 +0600

    lazy loading added

---
+++ standalone-lazy-demo/src/app/admin/admin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  imports: [RouterModule],
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrl: './admin.component.css'
+})
+export class AdminComponent {}
+
--- standalone-lazy-demo/src/main.ts
+++ standalone-lazy-demo/src/main.ts
@@ -1,6 +1,21 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { provideRouter, Routes } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'admin',
+    loadComponent: () => import('./app/admin/admin.component').then(m => m.AdminComponent),
+    children: [
+      { path: 'dashboard', loadComponent: () => import('./app/admin/dashboard/dashboard.component').then(m => m.DashboardComponent) },
+      { path: 'settings', loadComponent: () => import('./app/admin/settings/settings.component').then(m => m.SettingsComponent) },
+    ],
+  },
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)],
+});
+
